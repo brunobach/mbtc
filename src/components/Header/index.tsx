1676f809027c0fa856da8edda2711e0f29d385ff
Bruno Bach <42494225+brunobach@users.noreply.github.com>
2021-10-13 10:15:46 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
   return (
     <Container>
       <a className="User-link" href="https://github.com/maratonadev-br/desafio-3-2020" title="Link do Desafio 3">
-        <img src="https://maratona.dev/static/img/logo-pt.png" width="300" alt="MBTC" />
+        <img src="https://www.maratona.dev/static/logos/maratona.png" width="300" alt="MBTC" />
       </a>
       <User>
         <a href="https://github.com/brunobach/" title="Perfil Bruno Bach github">
